e00a34ec585b25a33b329a1984af0c719d8ced8f
Dmitriy Belinskiy <dmitriybelinskiy@gmail.com>
2017-11-25 15:29:33 +0300

    Added template

--- Doubly_linked_list/DlinkedList_class.h
+++ Doubly_linked_list/DlinkedList_class.h
@@ -1,25 +1,28 @@
-//Header file with double linked list class
-//TODO: template <typename T>
+//Header file with double linked list template class and its realization
+
 #pragma once
 #include <iostream>
 #include <iomanip>
+using namespace std;
 
+template <class T>
 struct Unit {
 	Unit *next; //Pointer to the adress of the next Unit in the list
 	Unit *previous; //Pointer to the adress of the previous Unit in the list
-	int stored_data;
+	T stored_data;
 };
 
+template <class T>
 class DoublyLinkedList {
 private:
-	Unit *p_head; //Pointer on the begining of the list
-	Unit *p_tail; //Pointer to the current adress
+	Unit<T> *p_head; //Pointer on the begining of the list
+	Unit<T> *p_tail; //Pointer to the current adress
 
 
 public:
 	DoublyLinkedList(); //Creating empty list
 	~DoublyLinkedList(); //Deleting all allocated memory
-	void Push_back(const int); //Adding new element to the end of the list
+	void Push_back(const T); //Adding new element to the end of the list
 	void Pop_back(); //moving back through the list and printing out stored data
 	void Pop_forward(); //moving forward through the list and printing out stored data
 	void Unit_Delete(); //Deleting one element
@@ -27,3 +30,91 @@ public:
 	void insert(int); //inserting element in to the list
 	*/
 };
+
+
+
+//Creating empty list
+template <class T>
+DoublyLinkedList<T>::DoublyLinkedList() : p_head(nullptr), p_tail(nullptr) {}
+
+//Deleting all allocated memory
+template <class T>
+DoublyLinkedList<T>::~DoublyLinkedList() {
+	//Moving to the end of the list
+	while (p_tail->next != nullptr) {
+		Unit<T> *temp = p_tail->next;
+		p_tail = temp;
+	}
+	while (p_tail != nullptr) {
+		Unit<T> *previous_temp = p_tail->previous;
+		delete[]p_tail;
+		p_tail = previous_temp;
+	}
+}
+
+//Adding new element to the end of the list
+template <class T>
+void DoublyLinkedList<T>::Push_back(const T input) {
+	//if list is empty
+	if (!(p_head)) {
+		Unit<T> *temp = new Unit<T>;
+		temp->stored_data = input;
+		temp->previous = nullptr;
+		temp->next = nullptr;
+		p_head = temp;
+		p_tail = temp;
+	}
+	//if list is not empty
+	else {
+		Unit<T> *temp = new Unit<T>;
+		p_tail->next = temp;
+		temp->stored_data = input;
+		temp->previous = p_tail;
+		temp->next = nullptr;
+		p_tail = temp;
+	}
+}
+//moving back through the list and printing out stored data
+template <class T>
+void DoublyLinkedList<T>::Pop_back() {
+	if (p_head == nullptr) {
+		cout << "List is empty." << endl;
+	}
+	else if (p_tail->previous == nullptr) {
+		cout << p_tail->stored_data << endl;
+		cout << "End of list." << endl;
+	}
+	else {
+		cout << p_tail->stored_data << endl;
+		Unit<T> *temp = p_tail->previous;
+		p_tail = temp;
+	}
+}
+//moving forward through the list and printing out stored data
+template <class T>
+void DoublyLinkedList<T>::Pop_forward() {
+	if (p_head == nullptr)
+		cout << "List is empty." << endl;
+	else if (p_tail->next == nullptr) {
+		cout << p_tail->stored_data << endl;
+		cout << "End of list." << endl;
+	}
+	else {
+		cout << p_tail->stored_data << endl;
+		Unit<T> *temp = p_tail->next;
+		p_tail = temp;
+	}
+}
+//Deleting one element
+template <class T>
+void DoublyLinkedList<T>::Unit_Delete() {
+	if (p_tail == nullptr)
+		cout << "List is empty." << endl;
+	else {
+		Unit<T> *previous_temp = p_tail->previous;
+		previous_temp->next = p_tail->next;
+		delete[]p_tail; //Deleting allocated memory
+		cout << "Unit was deleted." << endl;
+		p_tail = previous_temp;
+	}
+}
--- Doubly_linked_list/Doublylinkedlist.cpp
+++ Doubly_linked_list/Doublylinkedlist.cpp
@@ -1,26 +1,52 @@
 /*Creating double linked list.*/
 #include "stdafx.h"
 #include "DlinkedList_class.h" //Header with double linked list class
+using namespace std;
 
 int main()
 {
-	DoublyLinkedList test;
-	test.Pop_back();
+	//Int double linked list
+	cout << "------INT-------" << endl;
+		DoublyLinkedList <int> integer_list;
+	integer_list.Pop_back();
 	//filling list with data
 	for (int i = 0; i < 4; i++)
-		test.Push_back(i);
+		integer_list.Push_back(i);
 	//moving back through the list and printing out stored data
 	for (int i = 0; i < 4; i++)
-		test.Pop_back();
+		integer_list.Pop_back();
 	//moving forward through the list and printing out stored data
 	for (int i = 0; i < 4; i++)
-		test.Pop_forward();
+		integer_list.Pop_forward();
 	//Deleting one unit
-	test.Unit_Delete();
-	test.Unit_Delete();
+	integer_list.Unit_Delete();
+	integer_list.Unit_Delete();
 
-	test.Pop_back();	
-	test.Pop_back();
+	integer_list.Pop_back();
+	integer_list.Pop_back();
+
+	//double double linked list
+	cout << "-----DOUBLE------" << endl;
+	DoublyLinkedList <double> double_list;
+	double_list.Pop_back();
+	double_list.Push_back(1.4556);
+	double_list.Push_back(5.6754);
+	
+	double_list.Pop_back();
+	double_list.Pop_back();
+	std::cout << std::endl;
+
+	/*
+	cout << "-----STRING------" << endl;
+	//string double linked list
+	DoublyLinkedList <string> string_list;
+	string_list.Pop_back();
+	string_list.Push_back("one");
+	string_list.Push_back("two");
+
+	string_list.Pop_back();
+	string_list.Pop_back();
+	*/
 	
 	
 	return 0;
